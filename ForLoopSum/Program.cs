910e534f8abf2bc4b6df23ab71a4a4ac9c0eb134
MKaige <144488413+MKaige@users.noreply.github.com>
2023-09-18 14:31:55 +0300

    Add files via upload

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,22 @@
+﻿//numbrid 1-10ni
+//for(int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"i = {i + 1}");
+//    //{i + 1} ei muuda i väärtust vaid on lihtsalt kuvamiseks
+//}
+
+//numbrid 10-1ni
+//for (int i = 10; i > 0; i--)
+//{
+//    Console.WriteLine($"i = {i}");
+//   }
+
+//numbrite kokku lugemine
+int sum = 0;
+for(int i = 1; i < 11; i++)
+{
+    Console.WriteLine($"i = {i}");
+    Console.WriteLine($"Current total: {sum} ");
+    sum = sum + i;
+}
+Console.WriteLine($"Final Total {sum} ");
