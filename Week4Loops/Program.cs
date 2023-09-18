ff2ccac151488ab516bb19039e4d1557c9c200b9
MKaige <144488413+MKaige@users.noreply.github.com>
2023-09-18 14:32:44 +0300

    Add files via upload

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿// FOR-LOOP
+//DRY-principle = Do Not Repeat Yourself
+
+int i = 0;
+
+for (i = 0; i < 5; i++)
+//i on muutuja ning 0 näitab algust
+//i < 5 ehk kui mitu korda ja mis tingimuseni tsüklit kordab
+//i++  võrdub i+1, ehk samm 
+{
+    Console.WriteLine("We don´t need no education");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day!");
+
+
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,25 @@
+﻿// While-loop on tsükkel mis kestab kui mõni tingimus täitub
+
+Random rnd = new Random();
+int cpuRandom;
+bool loopActive = true; //bool väärtus saab olla true või false
+
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    //Console.WriteLine($"Cpu has generated {cpuRandom}.");
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+    int userNumber = Int32.Parse(Console.ReadLine()); // tuleb parssida numbriks, sest süsteem loeb automaatselt stringiks
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congrats, you won!");
+        //loopActive = false; // peatab tsükli vairant 1
+        break; // peatab tsükli variant 2
+    }
+    else
+    {
+        Console.WriteLine("Oops. Try again");
+    }
+}
+Console.WriteLine("Have a nice day!");
